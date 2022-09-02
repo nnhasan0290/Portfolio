2e5d995c760147532f22d399e0e53e2c2118e659
Nazmul Hasan <nazmulhasan02901@gmail.com>
2022-09-02 11:43:41 +0000

    menu

---
+++ components/Heading.jsx
@@ -0,0 +1,34 @@
+import {
+    Home, HomeRepairService, Mail, Person
+} from '@mui/icons-material';
+import Link from 'next/link';
+import React from 'react';
+
+function Heading() {
+  return (
+    <div className="text-white bg-[#111] fixed right-[30px] transform top-[50%] translate-y-[-50%]">
+      <ul className="flex flex-col">
+        <li className="icons-parent-list group">
+          <Home />
+          <span className="icon-text">HOME</span>
+        </li>
+        <Link href="/hoome">
+          <li className="whitespace-nowrap icons-parent-list group">
+            <Person />
+            <span className="icon-text">ABOUT ME</span>
+          </li>
+
+        </Link>
+        <li className="icons-parent-list group">
+          <HomeRepairService />
+          <span className="icon-text">PORTFOLIO</span>
+        </li>
+        <li className="text-white bg-[#2b2a2a]  rounded-full p-3 flex items-center justify-center m-4 whitespace-nowrap icons-parent-list group">
+          <Mail />
+          <span className="icon-text">CONTACT ME</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+export default Heading;
--- components/Home.jsx
+++ components/Home.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 function HomePage() {
   return (
     <main className="lg:flex sm:p-10 bg-[#111111] text-white box-border min-h-screen">
-      <div className="flex justify-center items-center px-10 py-2 w-full basis-1/3">
+      <div className="flex justify-center items-center px-10 py-2 sm:w-full w-[250px] basis-1/3 m-auto">
         <Image className="bg-[#e0dbdb] lg:rounded-md rounded-full border border-gray-500" src="/avatar.png" width="400px" height="400px" />
       </div>
       <div className="flex justify-center items-center basis-2/3">
@@ -18,7 +18,7 @@ function HomePage() {
             Frontend Developer
             {' '}
           </h1>
-          <p className="py-10 tracking-wider leading-8">
+          <p className="px-5 py-5 tracking-wider leading-8 md:py-10">
             I&apos;m a Tunisian based web designer & front‑end developer focused on crafting
             clean & user‑friendly experiences, I am passionate about building
             excellent software that improves the lives of those around me.
