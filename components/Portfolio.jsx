0c729bb43b3f3631af79b2cf0e99ec5197255344
nazmul <nazmulhasan02901@gmail.com>
2022-09-16 21:37:45 +0600

    graphcms

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import firstImg from '../public/Screenshot_1.jpg';
+import graphcms from '../public/graphcms.jpg';
 import secondImg from '../public/Screenshot_2.jpg';
 import thirdImg from '../public/Screenshot_3.jpg';
 import BigHead from './BigHead';
@@ -14,6 +14,10 @@ export default function Portfolio() {
     name: 'Simple Dashboard',
     tech: 'Next js, Chart Js, Mongoose',
   };
+  const cmsblog = {
+    name: 'Simple Blog',
+    tech: 'Next js, Graph CMS',
+  };
 
   return (
     <main className=" sm:p-10 bg-[#111111] text-white box-border min-h-screen pb-10 ">
@@ -21,6 +25,7 @@ export default function Portfolio() {
       <div className="sm:flex flex-wrap justify-center sm:mx-[70px]">
         <SinglePortfolio img={secondImg} details={shopgrid} link="https://nxt-shop.vercel.app/product" />
         <SinglePortfolio img={thirdImg} details={dashboard} link="https://nxt-shop.vercel.app/admin/dashboard" />
+        <SinglePortfolio img={graphcms} details={cmsblog} link="https://cms-blog-six-opal.vercel.app" />
       </div>
     </main>
   );
