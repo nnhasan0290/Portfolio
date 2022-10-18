a1ad9b40de459a7b48b3ccda3d41f3891a8c07bb
Nazmul Hasan <nazmulhasan02901@gmail.com>
2022-10-18 10:03:33 +0000

    env

--- firebase.js
+++ firebase.js
@@ -8,12 +8,12 @@ import {getFirestore} from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD9Sd17VP_XxBdLOMVFSUbWO8jwYyAn50M",
-  authDomain: "portfolio-e611c.firebaseapp.com",
+  apiKey: process.env.FIREBASEE_API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
   projectId: "portfolio-e611c",
   storageBucket: "portfolio-e611c.appspot.com",
   messagingSenderId: "1065376170400",
-  appId: "1:1065376170400:web:6033dbe73be177b9e3a4c5",
+  appId: process.env.APP_ID,
   measurementId: "G-8CDE465TWQ"
 };
 
