a0c8150732ed7e2a85820a46721da60c54a975ae
Nazmul Hasan <nazmulhasan02901@gmail.com>
2022-10-21 01:45:08 +0000

    blog-removed

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import graphcms from '../public/graphcms.jpg';
 import secondImg from '../public/Screenshot_2.jpg';
 import thirdImg from '../public/Screenshot_3.jpg';
 import tictac from "../public/tic.png";
@@ -16,10 +15,6 @@ export default function Portfolio() {
     name: 'Simple Dashboard',
     tech: 'Next js, Chart Js, Mongoose',
   };
-  const cmsblog = {
-    name: 'Simple Blog',
-    tech: 'Next js, Graph CMS, Graph QL',
-  };
   const tictactoe = {
     name: 'TIC TAC TOE',
     tech: 'Vanilla JavaScript, Vanilla Css',
@@ -35,7 +30,6 @@ export default function Portfolio() {
       <div className="sm:flex flex-wrap justify-center sm:mx-[70px]">
         <SinglePortfolio img={secondImg} details={shopgrid} link="https://nxt-shop.vercel.app/product" />
         <SinglePortfolio img={thirdImg} details={dashboard} link="https://nxt-shop.vercel.app/admin/dashboard" />
-        <SinglePortfolio img={graphcms} details={cmsblog} link="https://cms-blog-six-opal.vercel.app" />
         <SinglePortfolio img={tictac} details={tictactoe} link="https://tic-tac-toe-pearl-mu.vercel.app" />
         <SinglePortfolio img={basket} details={basketball} link="https://basketball-prediction.vercel.app" />
       </div>
