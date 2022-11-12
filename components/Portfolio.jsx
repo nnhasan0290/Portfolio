8cf70b4e0098c2ac654f2079a87a9266856beb01
Nazmul Hasan <nazmulhasan02901@gmail.com>
2022-11-12 04:12:41 +0000

    slider-cs

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -5,6 +5,8 @@ import tictac from "../public/tic.png";
 import basket from "../public/basketball.png";
 import BigHead from './BigHead';
 import SinglePortfolio from './SinglePortfolio';
+import Slider from "../public/slider.png";
+import cs from "../public/cs.png"
 
 export default function Portfolio() {
   const shopgrid = {
@@ -22,13 +24,23 @@ export default function Portfolio() {
   const basketball = {
     name: 'Basketball Winner Prediction',
     tech: 'React, LocalStorage, MUi'
-  }
+  };
+  const fixedslider = {
+    name: 'SVG slider',
+    tech: 'React, GSAP, SVG drawing'
+  };
+  const cscience = {
+    name: 'Static Figma Design',
+    tech: 'Next js, Figma, bootstrap'
+  };
 
   return (
     <main className=" sm:p-10 bg-[#111111] text-white box-border min-h-screen pb-10 ">
       <BigHead main="PORTFOLIO" background="WORKS" />
       <div className="sm:flex flex-wrap justify-center sm:mx-[70px]">
         <SinglePortfolio img={secondImg} details={shopgrid} link="https://nxt-shop.vercel.app/product" />
+        <SinglePortfolio img={cs} details={cscience} link="https://bn-asignment.vercel.app" />
+        <SinglePortfolio img={Slider} details={fixedslider} link="https://asignment-bn.vercel.app" />
         <SinglePortfolio img={thirdImg} details={dashboard} link="https://nxt-shop.vercel.app/admin/dashboard" />
         <SinglePortfolio img={tictac} details={tictactoe} link="https://tic-tac-toe-pearl-mu.vercel.app" />
         <SinglePortfolio img={basket} details={basketball} link="https://basketball-prediction.vercel.app" />
