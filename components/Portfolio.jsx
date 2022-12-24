4af6fa8c65198465d8f6950ceca2da78031e8535
nhasan <nazmulhasan02901@gmail.com>
2022-12-24 10:18:11 +0600

    main

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,49 +1,93 @@
-import React from 'react';
-import secondImg from '../public/Screenshot_2.jpg';
-import thirdImg from '../public/Screenshot_3.jpg';
+import React from "react";
+import secondImg from "../public/Screenshot_2.jpg";
+import thirdImg from "../public/Screenshot_3.jpg";
 import tictac from "../public/tic.png";
 import basket from "../public/basketball.png";
-import BigHead from './BigHead';
-import SinglePortfolio from './SinglePortfolio';
+import BigHead from "./BigHead";
+import SinglePortfolio from "./SinglePortfolio";
 import Slider from "../public/slider.png";
-import cs from "../public/cs.png"
+import cs from "../public/cs.png";
+import raghavImg from "../public/raghav.png";
+import shadamonImg from "../public/shadamon.png";
 
 export default function Portfolio() {
   const shopgrid = {
-    name: 'Shop Grid',
-    tech: 'Next js, Express js, Mongoose, JWT Authentication',
+    name: "Shop Grid",
+    tech: "Next js, Express js, Mongoose, JWT Authentication",
   };
   const dashboard = {
-    name: 'Simple Dashboard',
-    tech: 'Next js, Chart Js, Mongoose',
+    name: "Simple Dashboard",
+    tech: "Next js, Chart Js, Mongoose",
   };
   const tictactoe = {
-    name: 'TIC TAC TOE',
-    tech: 'Vanilla JavaScript, Vanilla Css',
+    name: "TIC TAC TOE",
+    tech: "Vanilla JavaScript, Vanilla Css",
   };
   const basketball = {
-    name: 'Basketball Winner Prediction',
-    tech: 'React, LocalStorage, MUi'
+    name: "Basketball Winner Prediction",
+    tech: "React, LocalStorage, MUi",
   };
   const fixedslider = {
-    name: 'SVG slider',
-    tech: 'React, GSAP, SVG drawing'
+    name: "SVG slider",
+    tech: "React, GSAP, SVG drawing",
   };
   const cscience = {
-    name: 'Static Figma Design',
-    tech: 'Next js, Figma, bootstrap'
+    name: "Static Figma Design",
+    tech: "Next js, Figma, bootstrap",
+  };
+  const raghav = {
+    name: "Build Service",
+    tech: "React js, Figma, Scroll Behavior",
+  };
+  const shadamon = {
+    name: "Shadamon",
+    tech: "Next js, Ant Design, Node js",
   };
 
   return (
     <main className=" sm:p-10 bg-[#111111] text-white box-border min-h-screen pb-10 ">
       <BigHead main="PORTFOLIO" background="WORKS" />
       <div className="sm:flex flex-wrap justify-center sm:mx-[70px]">
-        <SinglePortfolio img={secondImg} details={shopgrid} link="https://nxt-shop.vercel.app/product" />
-        <SinglePortfolio img={cs} details={cscience} link="https://bn-asignment.vercel.app" />
-        <SinglePortfolio img={Slider} details={fixedslider} link="https://asignment-bn.vercel.app" />
-        <SinglePortfolio img={thirdImg} details={dashboard} link="https://nxt-shop.vercel.app/admin/dashboard" />
-        <SinglePortfolio img={tictac} details={tictactoe} link="https://tic-tac-toe-pearl-mu.vercel.app" />
-        <SinglePortfolio img={basket} details={basketball} link="https://basketball-prediction.vercel.app" />
+        <SinglePortfolio
+          img={secondImg}
+          details={shopgrid}
+          link="https://nxt-shop.vercel.app/product"
+        />
+        <SinglePortfolio
+          img={cs}
+          details={cscience}
+          link="https://bn-asignment.vercel.app"
+        />
+        <SinglePortfolio
+          img={shadamonImg}
+          details={shadamon}
+          link="https://shadamon-three.vercel.app/"
+        />
+        <SinglePortfolio
+          img={raghavImg}
+          details={raghav}
+          link="https://atg-raghav.vercel.app/"
+        />
+        <SinglePortfolio
+          img={Slider}
+          details={fixedslider}
+          link="https://asignment-bn.vercel.app"
+        />
+        <SinglePortfolio
+          img={thirdImg}
+          details={dashboard}
+          link="https://nxt-shop.vercel.app/admin/dashboard"
+        />
+        <SinglePortfolio
+          img={tictac}
+          details={tictactoe}
+          link="https://tic-tac-toe-pearl-mu.vercel.app"
+        />
+        <SinglePortfolio
+          img={basket}
+          details={basketball}
+          link="https://basketball-prediction.vercel.app"
+        />
       </div>
     </main>
   );
